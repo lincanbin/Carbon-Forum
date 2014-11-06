4651b7933b81032dbcc74a1e36a4b3be574f9734
lincanbin <lincanbin@hotmail.com>
2014-11-07 02:24:33 +0800

    Bug

--- dashboard.php
+++ dashboard.php
@@ -11,12 +11,15 @@ switch ($Action) {
 	case 'Cache':
 		UpdateConfig(array(
 			'NumFiles' => $DB->single('SELECT count(ID) FROM '.$Prefix.'upload'),
-			'NumTopics' => $DB->single('SELECT count(ID) FROM '.$Prefix.'topics'),
+			'NumTopics' => $DB->single('SELECT count(*) FROM '.$Prefix.'topics WHERE IsDel=0'),
 			'NumPosts' => $DB->single('SELECT count(ID) FROM '.$Prefix.'posts')-$Config['NumTopics'],
 			'NumUsers' => $DB->single('SELECT count(ID) FROM '.$Prefix.'users'),
 			'NumTags' => $DB->single('SELECT count(ID) FROM '.$Prefix.'tags')
 			)
 		);
+		$DB->query('UPDATE '.$Prefix.'users u SET u.Topics=(SELECT count(*) FROM '.$Prefix.'topics t WHERE t.UserName=u.UserName),u.Replies=(SELECT count(*) FROM '.$Prefix.'posts p WHERE p.UserName=u.UserName and p.IsTopic=0),u.Followers=(SELECT count(*) FROM '.$Prefix.'favorites f WHERE f.FavoriteID=u.ID and Type=3)');
+		$DB->query('UPDATE '.$Prefix.'topics t SET t.Replies=(SELECT count(*) FROM '.$Prefix.'posts p WHERE p.TopicID=t.ID and p.IsTopic=0 and p.IsDel=0),t.Favorites=(SELECT count(*) FROM '.$Prefix.'favorites f WHERE f.FavoriteID=t.ID and Type=1)');
+		$DB->query('UPDATE '.$Prefix.'tags t SET t.TotalPosts=(SELECT count(*) FROM '.$Prefix.'posttags p WHERE p.TagID=t.ID),t.Followers=(SELECT count(*) FROM '.$Prefix.'favorites f WHERE f.FavoriteID=t.ID and Type=2)');
 		$CacheMessage = '缓存更新成功';
 		break;
 	
