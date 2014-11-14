ad2e3ad8eb9c70789b26198cb582f6108448031a
lincanbin <lincanbin@hotmail.com>
2014-11-14 12:42:57 +0800

    Fix Bug in Favorites when Favorites are empty.

--- favorite_tags.php
+++ favorite_tags.php
@@ -8,14 +8,13 @@ if($Page<0 || $Page==1){
 }
 if($Page == 0) $Page = 1;
 $TagsFollowing = $DB->query('SELECT * FROM '.$Prefix.'favorites force index(UsersFavorites) Where UserID=? and Type=2',array($CurUserID));
-
-$TopicIDArray = $DB->column('SELECT TopicID FROM '.$Prefix.'posttags force index(TagsIndex) Where TagID in (?) ORDER BY TopicID DESC LIMIT '.($Page-1)*$Config['TopicsPerPage'].','.$Config['TopicsPerPage'],ArrayColumn($TagsFollowing, 'FavoriteID'));
+$TopicIDArray=array();
+if($TagsFollowing)
+	$TopicIDArray = $DB->column('SELECT TopicID FROM '.$Prefix.'posttags force index(TagsIndex) Where TagID in (?) ORDER BY TopicID DESC LIMIT '.($Page-1)*$Config['TopicsPerPage'].','.$Config['TopicsPerPage'],ArrayColumn($TagsFollowing, 'FavoriteID'));
 array_unique($TopicIDArray);
 $TopicsArray=array();
 if($TopicIDArray)
-{
 	$TopicsArray = $DB->query('SELECT `ID`, `Topic`, `Tags`, `UserID`, `UserName`, `LastName`, `LastTime`, `Replies` FROM '.$Prefix.'topics force index(PRI) Where ID in (?) and IsDel=0 ORDER BY LastTime DESC',$TopicIDArray);
-}
 
 $DB->CloseConnection();
 $PageTitle = '我关注的话题';
--- favorite_users.php
+++ favorite_users.php
@@ -7,8 +7,11 @@ if($Page<0 || $Page==1){
 	exit;
 }
 if($Page == 0) $Page = 1;
+$UsersFollowing = array();
 $UsersFollowing = $DB->query('SELECT * FROM '.$Prefix.'favorites force index(UsersFavorites) Where UserID=? and Type=3',array($CurUserID));
-$PostsArray = $DB->query('SELECT * FROM '.$Prefix.'posts force index(UserPosts) Where UserName in (?) and IsDel=0 ORDER BY PostTime DESC LIMIT '.($Page-1)*$Config['PostsPerPage'].','.$Config['PostsPerPage'], ArrayColumn($UsersFollowing, 'Title'));
+$PostsArray = array();
+if($UsersFollowing)
+	$PostsArray = $DB->query('SELECT * FROM '.$Prefix.'posts force index(UserPosts) Where UserName in (?) and IsDel=0 ORDER BY PostTime DESC LIMIT '.($Page-1)*$Config['PostsPerPage'].','.$Config['PostsPerPage'], ArrayColumn($UsersFollowing, 'Title'));
 $DB->CloseConnection();
 $PageTitle = '我关注的用户';
 $PageTitle .= $Page>1?' Page'.$Page:'';
