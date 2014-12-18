4ced6ce19d0733040bcde614cf8841fafcdc079d
lincanbin <lincanbin@hotmail.com>
2014-12-18 17:03:59 +0800

    Mobile Version: Favorite Tags.

---
+++ styles/mobile/template/favorite_tags.php
@@ -0,0 +1,75 @@
+<?php
+if (!defined('InternalAccess')) exit('error: 403 Access Denied');
+?>
+<!-- main-content start -->
+<div class="main-content">
+	<div class="title">
+		<a href="<?php echo $Config['WebsitePath']; ?>/">
+			<?php echo $Config['SiteName']; ?>
+		</a>
+		&raquo; <?php echo $Lang['My_Following_Tags']; ?>
+	</div>
+	<div class="main-box home-box-list">
+		<?php
+		foreach ($TopicsArray as $Topic) {
+		?>
+		<div class="post-list">
+			<div class="item-avatar">
+				<a href="<?php echo $Config['WebsitePath']; ?>/u/<?php echo $Topic['UserName'] ?>" target="_blank">
+						<?php echo GetAvatar($Topic['UserID'], $Topic['UserName'], 'middle'); ?>
+					</a>
+			</div>
+			<div class="item-content">
+				<h2>
+					<a href="<?php echo $Config['WebsitePath']; ?>/t/<?php echo $Topic['ID']; ?>" target="_blank"><?php echo $Topic['Topic']; ?></a>
+				</h2>
+				<span class="item-tags">
+					<?php
+					if($Topic['Tags']){
+						foreach (explode("|", $Topic['Tags']) as $Tag) {
+						?><a href="<?php echo $Config['WebsitePath']; ?>/tag/<?php echo urlencode($Tag); ?>" target="_blank"><?php echo $Tag; ?></a>
+						<?php
+						}
+					}
+					?>
+				</span>
+				<span class="item-date float-right">
+					<a href="<?php echo $Config['WebsitePath']; ?>/u/<?php echo $Topic['UserName'] ?>" target="_blank"><?php echo $Topic['UserName']; ?></a>&nbsp;•&nbsp;
+					<?php echo FormatTime($Topic['LastTime']); 
+					if($Topic['Replies']){
+					?>&nbsp;•&nbsp;<?php echo $Lang['Last_Reply_From']; ?>&nbsp;<a href="<?php echo $Config['WebsitePath']; ?>/u/<?php echo $Topic['LastName'] ?>" target="_blank"><?php echo $Topic['LastName']; ?></a>
+					<?php } ?>
+				</span>
+			</div>
+		<?php if($Topic['Replies']){ ?>
+			<div class="item-count">
+				<a href="<?php echo $Config['WebsitePath']; ?>/t/<?php echo $Topic['ID']; ?>"><?php echo $Topic['Replies']; ?></a>
+			</div>
+		<?php } ?>
+			<div class="c"></div>
+		</div>
+		<?php
+		}
+		?>
+		<div class="pagination">
+			<?php PaginationSimplified('/tags/following/page/', $Page, empty($TopicsArray)); ?>
+			<div class="c"></div>
+		</div>
+	</div>
+</div>
+<!-- main-content end -->
+<!-- main-sider start -->
+<div class="main-sider">
+	<div class="sider-box">
+		<div class="sider-box-title"><?php echo $Lang['My_Following_Tags']; ?></div>
+		<div class="sider-box-content btn">
+			<?php foreach ($TagsFollowing as $Tag) {?>
+			<a href="<?php echo $Config['WebsitePath']; ?>/tag/<?php echo urlencode($Tag['Title']); ?>" target="_blank"><?php echo $Tag['Title']; ?></a>
+			<?php } ?>
+		</div>
+	</div>
+	<?php
+	include($TemplatePath.'sider.php');
+	?>
+</div>
+<!-- main-sider end -->
