6c8e754dd239d68426e5589ceb1ba68d9834ec8f
lincanbin <lincanbin@hotmail.com>
2015-02-06 21:33:29 +0800

    Fool-proofing in Advanced Settings. #14

--- dashboard.php
+++ dashboard.php
@@ -1,37 +1,43 @@
 <?php
 include(dirname(__FILE__) . '/common.php');
-require(dirname(__FILE__).'/language/'.ForumLanguage.'/dashboard.php');
+require(dirname(__FILE__) . '/language/' . ForumLanguage . '/dashboard.php');
 Auth(5);
-$BasicMessage = '';
-$PageMessage = '';
+$BasicMessage    = '';
+$PageMessage     = '';
 $AdvancedMessage = '';
-$CacheMessage = '';
-$Action = Request('POST','Action',false);
+$CacheMessage    = '';
+$Action          = Request('POST', 'Action', false);
 
 switch ($Action) {
 	case 'Cache':
 		UpdateConfig(array(
-			'NumFiles' => $DB->single('SELECT count(ID) FROM '.$Prefix.'upload'),
-			'NumTopics' => $DB->single('SELECT count(*) FROM '.$Prefix.'topics WHERE IsDel=0'),
-			'NumPosts' => $DB->single('SELECT sum(Replies) FROM '.$Prefix.'topics WHERE IsDel=0'),
-			'NumUsers' => $DB->single('SELECT count(ID) FROM '.$Prefix.'users'),
-			'NumTags' => $DB->single('SELECT count(ID) FROM '.$Prefix.'tags')
-			)
-		);
-		$DB->query('UPDATE '.$Prefix.'users u SET u.Topics=(SELECT count(*) FROM '.$Prefix.'topics t WHERE t.UserName=u.UserName),u.Replies=(SELECT count(*) FROM '.$Prefix.'posts p WHERE p.UserName=u.UserName and p.IsTopic=0),u.Followers=(SELECT count(*) FROM '.$Prefix.'favorites f WHERE f.FavoriteID=u.ID and Type=3)');
-		$DB->query('UPDATE '.$Prefix.'topics t SET t.Replies=(SELECT count(*) FROM '.$Prefix.'posts p WHERE p.TopicID=t.ID and p.IsTopic=0 and p.IsDel=0),t.Favorites=(SELECT count(*) FROM '.$Prefix.'favorites f WHERE f.FavoriteID=t.ID and Type=1)');
-		$DB->query('UPDATE '.$Prefix.'tags t SET t.TotalPosts=(SELECT count(*) FROM '.$Prefix.'posttags p WHERE p.TagID=t.ID),t.Followers=(SELECT count(*) FROM '.$Prefix.'favorites f WHERE f.FavoriteID=t.ID and Type=2)');
+			'NumFiles' => $DB->single('SELECT count(ID) FROM ' . $Prefix . 'upload'),
+			'NumTopics' => $DB->single('SELECT count(*) FROM ' . $Prefix . 'topics WHERE IsDel=0'),
+			'NumPosts' => $DB->single('SELECT sum(Replies) FROM ' . $Prefix . 'topics WHERE IsDel=0'),
+			'NumUsers' => $DB->single('SELECT count(ID) FROM ' . $Prefix . 'users'),
+			'NumTags' => $DB->single('SELECT count(ID) FROM ' . $Prefix . 'tags')
+		));
+		$DB->query('UPDATE ' . $Prefix . 'users u SET u.Topics=(SELECT count(*) FROM ' . $Prefix . 'topics t WHERE t.UserName=u.UserName),u.Replies=(SELECT count(*) FROM ' . $Prefix . 'posts p WHERE p.UserName=u.UserName and p.IsTopic=0),u.Followers=(SELECT count(*) FROM ' . $Prefix . 'favorites f WHERE f.FavoriteID=u.ID and Type=3)');
+		$DB->query('UPDATE ' . $Prefix . 'topics t SET t.Replies=(SELECT count(*) FROM ' . $Prefix . 'posts p WHERE p.TopicID=t.ID and p.IsTopic=0 and p.IsDel=0),t.Favorites=(SELECT count(*) FROM ' . $Prefix . 'favorites f WHERE f.FavoriteID=t.ID and Type=1)');
+		$DB->query('UPDATE ' . $Prefix . 'tags t SET t.TotalPosts=(SELECT count(*) FROM ' . $Prefix . 'posttags p WHERE p.TagID=t.ID),t.Followers=(SELECT count(*) FROM ' . $Prefix . 'favorites f WHERE f.FavoriteID=t.ID and Type=2)');
 		$CacheMessage = $Lang['Successfully_Refreshed'];
 		break;
 	
 	default:
 		$NewConfig = $_POST;
-		foreach ($NewConfig as $Key => $Value)
-		{
-			if(!array_key_exists($Key, $Config) || $Value == $Config[$Key])
-			{
+		//Fool-proofing
+		if($Action == 'Advanced'){
+			if($NewConfig['MobileDomainName'] == $_SERVER ['HTTP_HOST']){
+				$NewConfig['MobileDomainName'] = $Config['MobileDomainName'];
+			}
+			if($NewConfig['AppDomainName'] == $_SERVER ['HTTP_HOST']){
+				$NewConfig['AppDomainName'] = $Config['AppDomainName'];
+			}
+		}
+		foreach ($NewConfig as $Key => $Value) {
+			if (!array_key_exists($Key, $Config) || $Value == $Config[$Key]) {
 				unset($NewConfig[$Key]);
-			}else{
+			} else {
 				$Config[$Key] = $NewConfig[$Key];
 			}
 		}
@@ -54,7 +60,6 @@ switch ($Action) {
 
 $DB->CloseConnection();
 // 页面变量
-$PageTitle = $Lang['System_Settings'];
-$ContentFile = $TemplatePath.'dashboard.php';
-include($TemplatePath.'layout.php');
-?>
+$PageTitle   = $Lang['System_Settings'];
+$ContentFile = $TemplatePath . 'dashboard.php';
+include($TemplatePath . 'layout.php');
