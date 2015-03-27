bf05a7f4a386cd77789a803a7db675332ef9f782
lincanbin <lincanbin@hotmail.com>
2015-03-28 01:13:50 +0800

    Add Memcache(d) #16: Clear unread marks and refresh cache.

--- notifications.php
+++ notifications.php
@@ -1,11 +1,13 @@
 <?php
-require(dirname(__FILE__)."/common.php");
-require(dirname(__FILE__).'/language/'.ForumLanguage.'/notifications.php');
+require(dirname(__FILE__) . '/common.php');
+require(dirname(__FILE__) . '/language/' . ForumLanguage . '/notifications.php');
 Auth(1);
-$NotificationsArray = $DB->query('SELECT n.ID as NID, n.Type, n.IsRead, p.ID, p.TopicID, p.IsTopic, p.UserID, p.UserName, p.Subject, p.Content, p.PostTime, p.IsDel FROM '.$Prefix.'notifications n LEFT JOIN '.$Prefix.'posts p on p.ID=n.PostID Where n.UserID = ? ORDER BY n.Time DESC LIMIT 200',array($CurUserID));
-$ReplyArray = array();
-$MentionArray = array();
-if($NotificationsArray){
+$NotificationsArray = $DB->query('SELECT n.ID as NID, n.Type, n.IsRead, p.ID, p.TopicID, p.IsTopic, p.UserID, p.UserName, p.Subject, p.Content, p.PostTime, p.IsDel FROM ' . $Prefix . 'notifications n LEFT JOIN ' . $Prefix . 'posts p on p.ID=n.PostID Where n.UserID = ? ORDER BY n.Time DESC LIMIT 200', array(
+	$CurUserID
+));
+$ReplyArray         = array();
+$MentionArray       = array();
+if ($NotificationsArray) {
 	foreach ($NotificationsArray as $Value) {
 		switch ($Value['Type']) {
 			case 1:
@@ -20,11 +22,13 @@ if($NotificationsArray){
 	}
 }
 unset($NotificationsArray);
-$DB->query("UPDATE ".$Prefix."users force index(PRI) SET NewMessage = 0 Where ID=:UserID",array('UserID'=>$CurUserID));
+//Clear unread marks
+UpdateUserInfo(array(
+	'NewMessage' => 0
+));
 $CurUserInfo['NewMessage'] = 0;
 $DB->CloseConnection();
 // 页面变量
-$PageTitle = $Lang['Notifications'];
-$ContentFile = $TemplatePath.'notifications.php';
-include($TemplatePath.'layout.php');
-?>
+$PageTitle   = $Lang['Notifications'];
+$ContentFile = $TemplatePath . 'notifications.php';
+include($TemplatePath . 'layout.php');
