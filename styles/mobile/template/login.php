e1cb8be9bf1e1640554d694c1645410261dbe735
lincanbin <lincanbin@hotmail.com>
2015-04-17 17:26:14 +0800

    Mobile version of forum #21: Login Page.

--- styles/mobile/template/login.php
+++ styles/mobile/template/login.php
@@ -16,43 +16,35 @@ if($error){
 <?php
 }
 ?>
+		<br />
 		<form action="?" method="post" onsubmit="JavaScript:this.Password.value=md5(this.Password.value);">
-			<input type="hidden" value="<?php echo $ReturnUrl; ?>" name="ReturnUrl" />
-			<input type="hidden" name="FormHash" value="<?php echo $FormHash; ?>" />
-			<table cellpadding="5" cellspacing="8" border="0" width="100%" class="fs14">
-				<tbody>
-					<tr>
-						<td width="30%" align="right"><?php echo $Lang['UserName']; ?></td>
-						<td width="auto" align="left"><input type="text" name="UserName" class="sl w200" value="<?php echo htmlspecialchars($UserName); ?>" /></td>
-					</tr>
-					<tr>
-						<td width="30%" align="right"><?php echo $Lang['Password']; ?></td>
-						<td width="auto" align="left"><input type="password" name="Password" class="sl w200" value="" /></td>
-					</tr>
-					<tr>
-						<td width="30%" align="right"><?php echo $Lang['Login_Expiration_Time']; ?></td>
-						<td width="auto" align="left">
-							<select name="Expires">
-								<option value="30">30<?php echo $Lang['Days']; ?></option>
-								<option value="14">14<?php echo $Lang['Days']; ?></option>
-								<option value="7">7<?php echo $Lang['Days']; ?></option>
-								<option value="1">1<?php echo $Lang['Days']; ?></option>
-								<option value="0">0<?php echo $Lang['Days']; ?></option>
-							</select>
-						</td>
-					</tr>
-					<tr>
-						<td width="30%" align="right"><?php echo $Lang['Verification_Code']; ?></td>
-						<td width="auto" align="left">
-							<label><input type="text" name="VerifyCode" class="w100" onclick="document.getElementById('Verification_Code_Img').src='<?php echo $Config['WebsitePath']; ?>/seccode.php';" value="" placeholder="<?php echo $Lang['Verification_Code']; ?>" /></label> <img src="" id="Verification_Code_Img" style="cursor: pointer;" onclick="this.src+=''" align="absmiddle" />
-						</td>
-					</tr>
-					<tr>
-						<td width="30%" align="right"></td>
-						<td width="auto" align="left"><input type="submit" value="<?php echo $Lang['Log_In']; ?>" name="submit" class="textbtn" />&nbsp;&nbsp;&nbsp;&nbsp;<a href="<?php echo $Config['WebsitePath']; ?>/register"><?php echo $Lang['Sign_Up']; ?></a></td>
-					</tr>
-				</tbody>
-			</table>
+			<div class="input-group">
+				<input type="hidden" value="<?php echo $ReturnUrl; ?>" name="ReturnUrl" />
+				<input type="hidden" name="FormHash" value="<?php echo $FormHash; ?>" />
+
+				<label for="UserName"><?php echo $Lang['UserName']; ?></label>
+				<input type="text" name="UserName" id="UserName" value="<?php echo htmlspecialchars($UserName); ?>" />
+				<br style="clear:both">
+				<label for="Password"><?php echo $Lang['Password']; ?></label>
+				<input type="password" name="Password" id="Password" value="" />
+				<br style="clear:both">
+				<label for="Expires"><?php echo $Lang['Login_Expiration_Time']; ?></label>
+				<select name="Expires" id="Expires" style="display:inline;">
+					<option value="30">30<?php echo $Lang['Days']; ?></option>
+					<option value="14">14<?php echo $Lang['Days']; ?></option>
+					<option value="7">7<?php echo $Lang['Days']; ?></option>
+					<option value="1">1<?php echo $Lang['Days']; ?></option>
+					<option value="0">0<?php echo $Lang['Days']; ?></option>
+				</select>
+				<br style="clear:both">
+				<label for="VerifyCode"><?php echo $Lang['Verification_Code']; ?></label>
+				<input type="text" name="VerifyCode" id="VerifyCode" onclick="document.getElementById('Verification_Code_Img').src='<?php echo $Config['WebsitePath']; ?>/seccode.php';" value="" placeholder="<?php echo $Lang['Verification_Code']; ?>"  style="width:33%;"/>
+				<img src="" id="Verification_Code_Img" style="cursor: pointer;" onclick="this.src+=''" align="absmiddle" />
+				<br style="clear:both">
+				<a href="<?php echo $Config['WebsitePath']; ?>/register" class="button"><?php echo $Lang['Sign_Up']; ?></a>
+				<input type="submit" class="button" value="<?php echo $Lang['Log_In']; ?>" name="submit" style="float:right;" />
+				
+			</div>
 		</form>
 	<!-- main-content end -->
 <?php
