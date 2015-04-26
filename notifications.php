d220ea987479bed4d8b3a9888339e0c0f956aa0e
lincanbin <lincanbin@hotmail.com>
2015-04-26 11:27:12 +0800

    Adjust the number of notifications.

--- notifications.php
+++ notifications.php
@@ -2,26 +2,21 @@
 require(dirname(__FILE__) . '/common.php');
 require(dirname(__FILE__) . '/language/' . ForumLanguage . '/notifications.php');
 Auth(1);
-$NotificationsArray = $DB->query('SELECT n.ID as NID, n.Type, n.IsRead, p.ID, p.TopicID, p.IsTopic, p.UserID, p.UserName, p.Subject, p.Content, p.PostTime, p.IsDel FROM ' . $Prefix . 'notifications n LEFT JOIN ' . $Prefix . 'posts p on p.ID=n.PostID Where n.UserID = ? ORDER BY n.Time DESC LIMIT 200', array(
+$ReplyArray   = $DB->query('SELECT n.ID as NID, n.Type, n.IsRead, p.ID, p.TopicID, p.IsTopic, p.UserID, p.UserName, p.Subject, p.Content, p.PostTime, p.IsDel 
+							FROM ' . $Prefix . 'notifications n LEFT JOIN ' . $Prefix . 'posts p 
+							on p.ID=n.PostID 
+							Where n.UserID = ? and n.Type=1 
+							ORDER BY n.Time DESC LIMIT 30', array(
 	$CurUserID
 ));
-$ReplyArray         = array();
-$MentionArray       = array();
-if ($NotificationsArray) {
-	foreach ($NotificationsArray as $Value) {
-		switch ($Value['Type']) {
-			case 1:
-				$ReplyArray[] = $Value;
-				break;
-			case 2:
-				$MentionArray[] = $Value;
-				break;
-			default:
-				break;
-		}
-	}
-}
-unset($NotificationsArray);
+$MentionArray = $DB->query('SELECT n.ID as NID, n.Type, n.IsRead, p.ID, p.TopicID, p.IsTopic, p.UserID, p.UserName, p.Subject, p.Content, p.PostTime, p.IsDel 
+							FROM ' . $Prefix . 'notifications n 
+							LEFT JOIN ' . $Prefix . 'posts p 
+							on p.ID=n.PostID 
+							Where n.UserID = ? and n.Type=2 
+							ORDER BY n.Time DESC LIMIT 30', array(
+	$CurUserID
+));
 //Clear unread marks
 UpdateUserInfo(array(
 	'NewMessage' => 0
