e882d845cafe0f43639f314a82a74a8ab6a28a65
lincanbin <lincanbin@hotmail.com>
2015-05-03 01:02:02 +0800

    Update program.

--- update/index.php
+++ update/index.php
@@ -2,8 +2,9 @@
 set_time_limit(0);
 date_default_timezone_set('Asia/Shanghai');//设置中国时区
 $Message = '';
+$Version = '3.3.0';
+
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
-	$fp = fopen(dirname(__FILE__).'/database.sql', "r") or die("不能打开SQL文件");
 	$Language = $_POST['Language'];
 	$DBHost = $_POST['DBHost'];
 	$DBName = $_POST['DBName'];
@@ -11,7 +12,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	$DBPassword = $_POST['DBPassword'];
 	//$WebsitePath = $_POST['WebsitePath'];
 	$WebsitePath = $_SERVER['PHP_SELF'] ? $_SERVER['PHP_SELF'] : $_SERVER['SCRIPT_NAME'];
-	if(preg_match('/(.*)\/install/i', $WebsitePath , $WebsitePathMatch))
+	if(preg_match('/(.*)\/update/i', $WebsitePath , $WebsitePathMatch))
 	{
 		$WebsitePath = $WebsitePathMatch[1];
 	}else{
@@ -20,31 +21,28 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	//初始化数据库操作类
 	require('../includes/PDO.class.php');
 	$DB = new Db($DBHost, $DBName, $DBUser, $DBPassword);
-	//数据库安装
-	while($SQL=GetNextSQL()){
-		$DB->query($SQL);
-	}
-	$DB->query("INSERT INTO `carbon_config` VALUES ('WebsitePath', '".$WebsitePath."')");
-	$DB->query("INSERT INTO `carbon_config` VALUES ('LoadJqueryUrl', '".$WebsitePath."/static/js/jquery.js')");
-	$DB->CloseConnection();
-	fclose($fp) or die("Can’t close file");
-
+	$OldVersion = $DB->single("SELECT ConfigValue `carbon_config` WHERE `ConfigName`='Version'");
+	//数据处理
+	$DB->query("UPDATE `carbon_config` SET `ConfigValue`='".$Version."' WHERE `ConfigName`='Version'");
+	$DB->query("UPDATE `carbon_config` SET `ConfigValue`='".$WebsitePath."' WHERE `ConfigName`='WebsitePath'");
+	$DB->query("UPDATE `carbon_config` SET `ConfigValue`='".$WebsitePath."/static/js/jquery.js' WHERE `ConfigName`='LoadJqueryUrl'");
+	
 	//写入config文件
-	$ConfigPointer=fopen(dirname(__FILE__).'/config.tpl','r');
-	$ConfigBuffer=fread($ConfigPointer, filesize(dirname(__FILE__).'/config.tpl'));
+	$ConfigPointer = fopen('../install/config.tpl','r');
+	$ConfigBuffer = fread($ConfigPointer, filesize(dirname(__FILE__).'/config.tpl'));
 	$ConfigBuffer = str_replace("{{Language}}",$Language,$ConfigBuffer);
 	$ConfigBuffer = str_replace("{{DBHost}}",$DBHost,$ConfigBuffer);
 	$ConfigBuffer = str_replace("{{DBName}}",$DBName,$ConfigBuffer);
 	$ConfigBuffer = str_replace("{{DBUser}}",$DBUser,$ConfigBuffer);
 	$ConfigBuffer = str_replace("{{DBPassword}}",$DBPassword,$ConfigBuffer);
 	fclose($ConfigPointer);
-	$ConfigPHP = fopen("../config.php","w+");       
+	$ConfigPHP = fopen('../config.php',"w+");       
 	fwrite($ConfigPHP,$ConfigBuffer);
 	fclose($ConfigPHP);
 
 	//写入htaccess文件
-	$HtaccessPointer=fopen(dirname(__FILE__).'/htaccess.tpl','r');
-	$HtaccessBuffer=fread($HtaccessPointer, filesize(dirname(__FILE__).'/htaccess.tpl'));
+	$HtaccessPointer=fopen('../install/htaccess.tpl','r');
+	$HtaccessBuffer=fread($HtaccessPointer, filesize('../install/htaccess.tpl'));
 	$HtaccessBuffer = str_replace("{{WebSitePath}}",$WebsitePath,$HtaccessBuffer);
 	//Server Software Type
 	if(isset($_SERVER['HTTP_X_REWRITE_URL'])){//IIS(ISAPI_Rewrite)
@@ -58,13 +56,32 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	fclose($Htaccess);
 
 	//rewrite文件配置
-	$Message = '安装成功，安装完成后请马上删除install文件夹。<br />Please delete the install folder. <br /><a href="../register">点我马上注册管理员账号<br />The first registered users will become administrators.</a>';
-
+	$Message = '升级成功，升级完成后请马上删除install和update文件夹。<br />Please delete the install folder and the update floder. ';
+	//当前版本低于3.3.0，需要进行的升级到3.3.0的升级操作
+	if(VersionCompare('3.3.0' ,$OldVersion)){
+		$UserIDArray = $DB->query('SELECT UserName, ID FROM '.$Prefix.'users');
+		foreach ($UserIDArray as $UserInfo) {
+			if(!is_file('../upload/avatar/small/' . $UserInfo['ID'] . '.png')){
+				echo $UserInfo['UserName'].'<br />';
+				if(extension_loaded('gd')){
+					$Avatar = new MDAvtars(mb_substr($UserInfo['UserName'], 0, 1, "UTF-8"), 256);
+					$Avatar->Save('../upload/avatar/large/' . $UserInfo['ID'] . '.png', 256);
+					$Avatar->Save('../upload/avatar/middle/' . $UserInfo['ID'] . '.png', 48);
+					$Avatar->Save('../upload/avatar/small/' . $UserInfo['ID'] . '.png', 24);
+				}
+			}
+		}
+	}
+	//3.3.0
+	//关闭数据库连接
+	$DB->CloseConnection();
 	//安全起见，修改为不可执行文件
 	if (file_exists('index.php')) {  
-		rename ("index.php", "index.txt");
+		rename("index.php", "index.txt");
 	}
-	
+	if (file_exists('../install/index.php')) {  
+		rename("../install/index.php", "../install/index.txt");
+	}
 }else{
 	if (version_compare(PHP_VERSION, '5.3.0') < 0) {
 		$Message = '你的PHP版本过低，可能会无法正常使用！<br />Your PHP version is too low, it may not work properly!';
@@ -73,32 +90,19 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 		$Message = '你的PHP未编译pdo_mysql，本程序无法正常工作<br />Your PHP don’t support pdo_mysql extension, this program does not work! ';
 	}
 }
-//从文件中逐条取SQL
-function GetNextSQL() {
-	global $fp;
-	$sql="";
-	while ($line = fgets($fp, 40960)) {
-		$line = trim($line);
-		//以下三句在高版本php中不需要，在部分低版本中也许需要修改
-		/*
-		$line = str_replace("////","//",$line);
-		$line = str_replace("/’","’",$line);
-		$line = str_replace("//r//n",chr(13).chr(10),$line);
-		*/
-		//$line = stripcslashes($line);
-		if (strlen($line)>1) {
-			if ($line[0]=="-" && $line[1]=="-") {
-				continue;
-			}
-		}
-		$sql.=$line.chr(13).chr(10);
-		if (strlen($line)>0){
-			if ($line[strlen($line)-1]==";"){
-				break;
-			}
+
+function VersionCompare($Version, $OldVersion)
+{
+	$VersionArray = array_map("intval", explode('.', $Version));;
+	$OldVersionArray = array_map("intval", explode('.', $OldVersion));
+	$NeedToUpdate = false;
+	foreach ($VersionArray as $Key => $Value) {
+		if($VersionArray[$Key] > $OldVersionArray[$Key]){
+			$NeedToUpdate = true;
+			break;
 		}
 	}
-	return $sql;
+	return $NeedToUpdate;
 }
 ?>
 
@@ -107,7 +111,7 @@ function GetNextSQL() {
 <head>
 <meta charset="UTF-8" />
 <meta content="True" name="HandheldFriendly" />
-<title>Install - Carbon Forum</title>
+<title>Update - Carbon Forum</title>
 <link href="../styles/default/theme/style.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
@@ -118,7 +122,7 @@ function GetNextSQL() {
 			<!-- main-content start -->
 <div class="main-content">
 	<div class="title">
-		Carbon Forum &raquo; 安装&nbsp;&nbsp;/&nbsp;&nbsp;Install
+		Carbon Forum &raquo; 升级&nbsp;&nbsp;/&nbsp;&nbsp;Update
 	</div>
 	<div class="main-box">
 			<form action="?" method="post">
@@ -156,7 +160,7 @@ function GetNextSQL() {
 					</tr>
 					<tr>
 						<td width="280" align="right"></td>
-						<td width="auto" align="left"><input type="submit" value="安 装 / Install " name="submit" class="textbtn" /></td>
+						<td width="auto" align="left"><input type="submit" value="升 级 / Update" name="submit" class="textbtn" /></td>
 					</tr>
 					<?php } ?>
 				</tbody>
@@ -167,13 +171,21 @@ function GetNextSQL() {
 	<!-- main-content end -->
 	<div class="main-sider">
 		<div class="sider-box">
-			<div class="sider-box-title">安装说明</div>
+			<div class="sider-box-title">升级说明</div>
 			<div class="sider-box-content">
-				<p>
-				如果出现“Access denied”错误，说明填写不正确，请返回重新填写。
+				<p class="red">
+				请一定要先备份好你的数据库和upload文件夹！！！<br />
+				如果没有备份，请不要继续进行升级操作。
+				</p>
+				<p class="red">
+				Please be sure to back up your database and upload folders! ! !<br />
+				If there is no backup, please do not proceed with the upgrade operation! ! !
 				</p>
 				<p>
-				安装完毕后，第一个注册的用户将会自动成为管理员。
+				如果出现“Access denied”错误说明填写不正确，请返回重新填写。
+				</p>
+				<p>
+				如果你使用的是性能极差的虚拟主机，你可以在本地电脑完成升级，再上传至服务器（注意本地电脑运行环境要与服务器一致）。
 				</p>
 			</div>
 		</div>
@@ -186,7 +198,7 @@ function GetNextSQL() {
 		<!-- footer start -->
 		<div class="Copyright">
 			<p>
-			Power By <a href="http://www.94cb.com" target="_blank">Carbon Forum</a> © 2006-2015
+			Power By <a href="http://www.94cb.com" target="_blank">Carbon Forum <?php echo $Version; ?></a> © 2006-2015
 			</p>
 		</div>
 		<!-- footer end -->
