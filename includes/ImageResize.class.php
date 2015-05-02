0d34b8f99cfe69046a45057b74ba7f4a6d87e821
lincanbin <lincanbin@hotmail.com>
2015-05-03 00:02:46 +0800

    Automatic extract the square central part of the avatar, and zoom to the specified size. https://github.com/lincanbin/PHP-Image-Resize-Class/commit/1ebb857f8827673683f8e76dbe9787207c1aaa05

--- includes/ImageResize.class.php
+++ includes/ImageResize.class.php
@@ -31,27 +31,26 @@ class ImageResize
 			if ($this->ImageSize) {
 				//创建源图片
 				/* 
-				$ImageTypeArray = array
-				(
-				0=>'UNKNOWN',
-				1=>'GIF',
-				2=>'JPEG',
-				3=>'PNG',
-				4=>'SWF',
-				5=>'PSD',
-				6=>'BMP',
-				7=>'TIFF_II',
-				8=>'TIFF_MM',
-				9=>'JPC',
-				10=>'JP2',
-				11=>'JPX',
-				12=>'JB2',
-				13=>'SWC',
-				14=>'IFF',
-				15=>'WBMP',
-				16=>'XBM',
-				17=>'ICO',
-				18=>'COUNT'  
+				$ImageTypeArray = array(
+					0=>'UNKNOWN',
+					1=>'GIF',
+					2=>'JPEG',
+					3=>'PNG',
+					4=>'SWF',
+					5=>'PSD',
+					6=>'BMP',
+					7=>'TIFF_II',
+					8=>'TIFF_MM',
+					9=>'JPC',
+					10=>'JP2',
+					11=>'JPX',
+					12=>'JB2',
+					13=>'SWC',
+					14=>'IFF',
+					15=>'WBMP',
+					16=>'XBM',
+					17=>'ICO',
+					18=>'COUNT'
 				);
 				$ImageType = $ImageTypeArray[$this->ImageSize[2]];
 				*/
@@ -75,19 +74,30 @@ class ImageResize
 	//bool Resize ( int $TargetMaxPx, string $TargetPath [, int $Quality ] )
 	public function Resize($TargetMaxPx, $TargetPath, $Quality = 95)
 	{
-		
 		if (isset($this->ImageObject)) {
-			$MaxImagePx = max($this->ImageSize[0], $this->ImageSize[1]);
+			$MinImagePx = min($this->ImageSize[0], $this->ImageSize[1]);
 			//chmod($TargetPath, 0777);
-			if ($MaxImagePx > $TargetMaxPx) {
-				$Percent      = $TargetMaxPx / $MaxImagePx;
-				$TargetWidth  = round($this->ImageSize[0] * $Percent);
-				$TargetHeight = round($this->ImageSize[1] * $Percent);
-				
-				$TargetImageData = imagecreatetruecolor($TargetWidth, $TargetHeight);
+			if ($MinImagePx > $TargetMaxPx) {
+				$Percent = 1.0;
+				if($this->ImageSize[0] > $this->ImageSize[1]){
+					$SourceX = round(($this->ImageSize[0] - $this->ImageSize[1])/2);
+					$SourceY = 0;
+					$Percent = 1.0 * $TargetMaxPx / $this->ImageSize[0];
+				}else{
+					$SourceX = 0;
+					$SourceY = round(($this->ImageSize[1] - $this->ImageSize[0])/2);
+					$Percent = 1.0 * $TargetMaxPx / $this->ImageSize[1];
+				}
+				//一个正方形图像，用来临时保存原图像中间部分
+				$TempImageData = imagecreatetruecolor($MinImagePx, $MinImagePx);
+				//将原图像中间部分拷贝到正方形中
+				imagecopy($TempImageData, $this->ImageObject, 0, 0, $SourceX, $SourceY, $MinImagePx, $MinImagePx);
+				//目标图像
+				$TargetImageData = imagecreatetruecolor($TargetMaxPx, $TargetMaxPx);
 				$Background      = imagecolorallocate($TargetImageData, 255, 255, 255);
 				imagefill($TargetImageData, 0, 0, $Background);
-				imagecopyresampled($TargetImageData, $this->ImageObject, 0, 0, 0, 0, $TargetWidth, $TargetHeight, $this->ImageSize[0], $this->ImageSize[1]);
+				//缩放
+				imagecopyresampled($TargetImageData, $TempImageData, 0, 0, 0, 0, $TargetMaxPx, $TargetMaxPx, $MinImagePx, $MinImagePx);
 				return imagejpeg($TargetImageData, $TargetPath, $Quality);
 			} else {
 				return imagejpeg($this->ImageObject, $TargetPath, $Quality);
@@ -97,6 +107,4 @@ class ImageResize
 		}
 		
 	}
-	
-	
 }
