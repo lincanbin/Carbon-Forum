7db08a780651a63de6a6c12fdf24a58d02d34ed3
LNDDYL <lnddyl@outlook.com>
2015-05-08 20:17:40 +0800

    Update user.php
    更新翻譯

--- language/zh-tw/user.php
+++ language/zh-tw/user.php
@@ -5,16 +5,16 @@ if (empty($Lang) || !is_array($Lang))
 
 $Lang = array_merge($Lang, array(
 	'Unfollow' => '取消關注',
-	'Follow' => '關注他',
+	'Follow' => '關注他/她',
 	'Block_User' => '停權',
 	'Unblock_User' => '復權',
-	'Reset_Avatar' => '重置肖像',
-	'Registered_In' => '申請於',
+	'Reset_Avatar' => '重設肖像',
+	'Registered_In' => '帳號申請於',
 	'Topics_Number' => '討論串數',
 	'Posts_Number' => '回覆數',
 	'Homepage' => '網站',
 	'Introduction' => '介紹',
 	'Last_Activity_In' => '最後活動於',
-	'Created_Topic' => '創建了討論串',
+	'Created_Topic' => '建立了討論串',
 	'Replied_To_Topic' => '回覆了討論串'
 	));
