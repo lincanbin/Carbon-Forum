b715d4e089aed29ae19b0f54bed7991863ff26e1
lincanbin <lincanbin@hotmail.com>
2015-07-20 00:43:37 +0800

    i18n #84: New FE Language Files.

---
+++ language/en/global.js
@@ -0,0 +1,37 @@
+/*
+ * Carbon-Forum
+ * https://github.com/lincanbin/Carbon-Forum
+ *
+ * Copyright 2006-2015 Canbin Lin (lincanbin@hotmail.com)
+ * http://www.94cb.com/
+ *
+ * Licensed under the Apache License, Version 2.0:
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * A high performance open-source forum software written in PHP. 
+ */
+var Lang = {
+	'Title_Can_Not_Be_Empty':'Title Can not be empty. ',
+	'Title_Too_Long':'Title length can not exceed {{MaxTitleChars}} bytes, current title length is {{Current_Title_Length}} bytes. ',
+	'Tags_Empty':'Tags can not be empty. ',
+	'Submitting':' Submitting ',
+	'Submit_Success':' Success ',
+	'Submit_Failure':'Failure, try again. ',
+	'Submit_Again':' Submit again ',
+	'Tags_Too_Much':'Up to {{MaxTagNum}} tags per topic. ',
+	'Add_Tags':'Add a tag (press Enter)',
+
+	'Edit':' Submit ',
+	'Cancel':' Cancel ',
+
+	'Confirm_Operation':'Please confirm the operation. ',
+	'Confirm':'Confirm',
+
+	'Reply':' Reply ',
+	'Content_Empty':'Content can not be empty! ',
+	'Replying':' Replying…… ',
+	'Reply_Success':' Success ',
+	'Reply_To':'Reply to ',
+
+	'New_Message': '{{NewMessage}} New Messages'
+};
---
+++ language/zh-tw/global.js
@@ -0,0 +1,37 @@
+/*
+ * Carbon-Forum
+ * https://github.com/lincanbin/Carbon-Forum
+ *
+ * Copyright 2006-2015 Canbin Lin (lincanbin@hotmail.com)
+ * http://www.94cb.com/
+ *
+ * Licensed under the Apache License, Version 2.0:
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * A high performance open-source forum software written in PHP. 
+ */
+var Lang = {
+	'Title_Can_Not_Be_Empty':'標題不能為空！',
+	'Title_Too_Long':'標題長度不能超過 {{MaxTitleChars}} 位元組，當前標題長度為 {{Current_Title_Length}} 個位元組',
+	'Tags_Empty':'標籤不能為空！',
+	'Submitting':' 發佈中 ',
+	'Submit_Success':' 發佈成功 ',
+	'Submit_Failure':'發表失敗，請再次送出！',
+	'Submit_Again':' 再次送出 ',
+	'Tags_Too_Much':'最多只能插入 {{MaxTagNum}} 個標籤！',
+	'Add_Tags':'新增標籤(按 Enter 新增)',
+
+	'Edit':' 送 出 ',
+	'Cancel':' 取 消 ',
+
+	'Confirm_Operation':'確認執行該操作？',
+	'Confirm':'確認',
+
+	'Reply':' 回 覆 ',
+	'Content_Empty':'內容不能為空！',
+	'Replying':' 正在回覆…… ',
+	'Reply_Success':' 回覆成功 ',
+	'Reply_To':'回覆',
+
+	'New_Message': '{{NewMessage}} 條訊息'
+};
