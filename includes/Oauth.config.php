60d885d562d6495e277011fde88feec172fe1555
lincanbin <lincanbin@hotmail.com>
2015-08-18 23:19:09 +0800

    Higher performance oAuth settings. #78

--- includes/Oauth.config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$OauthInfo  = array(
-	'QQ' => array(
-		'Name' => 'QQ'
-		'LogoUrl' => 'http://qzonestyle.gtimg.cn/qzone/vas/opensns/res/img/Connect_logo_1.png',
-		'ButtonImageUrl' => 'http://qzonestyle.gtimg.cn/qzone/vas/opensns/res/img/Connect_logo_7.png'
-	),
- );
