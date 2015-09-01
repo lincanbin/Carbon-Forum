37d8136830e4d1fa39892ecd06f46ab820ceecdc
lincanbin <lincanbin@hotmail.com>
2015-09-02 01:06:33 +0800

    https://developer.github.com/v3/#user-agent-required #78

--- includes/URL.class.php
+++ includes/URL.class.php
@@ -1,10 +1,11 @@
 <?php
 class URL{
 
-	public static function get($url){
+	public static function Get($url){
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0) CarbonForum/5.0");
 		curl_setopt($ch, CURLOPT_URL, $url);
 		$response =  curl_exec($ch);
 		curl_close($ch);
@@ -15,11 +16,12 @@ class URL{
 		}
 	}
 
-	public static function post($url, $data, $flag = 0){
+	public static function Post($url, $data, $flag = 0){
 
 		$ch = curl_init();
 		if(! $flag) curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); 
+		curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0) CarbonForum/5.0");
 		curl_setopt($ch, CURLOPT_POST, TRUE); 
 		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data)); 
 		curl_setopt($ch, CURLOPT_URL, $url);
