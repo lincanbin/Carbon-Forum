92222b616699fe57e4a414473afaabc9a28d26cf
lincanbin <lincanbin@hotmail.com>
2015-09-14 18:00:23 +0800

    Search. #92

--- styles/mobile/template/search.php
+++ styles/mobile/template/search.php
@@ -14,24 +14,12 @@ function KeywordHighlight($Content)
 		return $Content;
 	}
 }
-if(!$IsAjax){
 ?>
-<!-- this is the header div at the top -->
-<div id="header">
-	<a id="menubadge" onclick="JavaScript:af.ui.toggleSideMenu()" class="menuButton"></a>
-</div>
-<div id="content">
-	<!-- here is where you can add your panels -->
-<?php
-}
-?>
-	<div data-title="<?php echo $PageTitle; ?>" id="Search<?php echo base64_encode($Keyword).'-'.$Page; ?>" class="panel" selected="true">
-		<ul class="list topic-list">
+<ul class="list topic-list">
 <?php
 if($Page>1){
 ?>
-			<li class="pagination"><a href="JavaScript:$.ui.loadContent('<?php echo $Config['WebsitePath']; ?>/search/<?php echo urlencode($Keyword).'/page/'.($Page-1); ?>',false,false,'slide');" data-transition="slide" data-persist-ajax="true"><?php echo $Lang['Page_Previous']; ?></a></li>
-
+	<li class="pagination"><a href="JavaScript:$.ui.loadContent('<?php echo $Config['WebsitePath']; ?>/search/<?php echo urlencode($Keyword).'/page/'.($Page-1); ?>',false,false,'slide');" data-transition="slide" data-persist-ajax="true"><?php echo $Lang['Page_Previous']; ?></a></li>
 <?php
 }
 ?>
@@ -39,46 +27,33 @@ if($Page>1){
 <?php
 foreach ($TopicsArray as $Topic) {
 ?>
-			<li>
-				<div class="avatar">
-					<a href="<?php echo $Config['WebsitePath']; ?>/u/<?php echo urlencode($Topic['UserName']); ?>" data-transition="slide" data-persist-ajax="true">
-							<?php echo GetAvatar($Topic['UserID'], $Topic['UserName'], 'middle'); ?>
-					</a>
-				</div>
-				<div class="content">
-				<a href="<?php echo $Config['WebsitePath']; ?>/t/<?php echo $Topic['ID']; ?>" data-transition="slide" data-persist-ajax="true">
-					<h2><?php echo KeywordHighlight($Topic['Topic']); ?></h2>
-				</a>
-				<p><?php echo FormatTime($Topic['LastTime']); ?>&nbsp;&nbsp;<?php echo $Topic['LastName']; ?>
-				</p>
-				<?php if($Topic['Replies']){ ?>
-				<span class="aside">
-					<?php echo $Topic['Replies']; ?>
-				</span>
-				<?php } ?>
-				</div>
-				
-				<div class="c"></div>
-			</li>
+	<li>
+		<div class="avatar">
+			<a href="<?php echo $Config['WebsitePath']; ?>/u/<?php echo urlencode($Topic['UserName']); ?>" data-transition="slide" data-persist-ajax="true">
+					<?php echo GetAvatar($Topic['UserID'], $Topic['UserName'], 'middle'); ?>
+			</a>
+		</div>
+		<div class="content">
+		<a href="<?php echo $Config['WebsitePath']; ?>/t/<?php echo $Topic['ID']; ?>" data-transition="slide" data-persist-ajax="true">
+			<h2><?php echo KeywordHighlight($Topic['Topic']); ?></h2>
+		</a>
+		<p><?php echo FormatTime($Topic['LastTime']); ?>&nbsp;&nbsp;<?php echo $Topic['LastName']; ?>
+		</p>
+		<?php if($Topic['Replies']){ ?>
+		<span class="aside">
+			<?php echo $Topic['Replies']; ?>
+		</span>
+		<?php } ?>
+		</div>
+		
+		<div class="c"></div>
+	</li>
 <?php
 }
 if(!empty($TopicsArray)){
 ?>
-			<li class="pagination"><a href="JavaScript:$.ui.loadContent('<?php echo $Config['WebsitePath']; ?>/search/<?php echo urlencode($Keyword).'/page/'.($Page+1); ?>',false,false,'slide');" data-transition="slide" data-persist-ajax="false"><?php echo $Lang['Page_Next']; ?></a></li>
+	<li class="pagination"><a href="JavaScript:$.ui.loadContent('<?php echo $Config['WebsitePath']; ?>/search/<?php echo urlencode($Keyword).'/page/'.($Page+1); ?>',false,false,'slide');" data-transition="slide" data-persist-ajax="false"><?php echo $Lang['Page_Next']; ?></a></li>
 <?php
 }
 ?>
-		</ul>
-	</div>
-<?php
-if(!$IsAjax){
-?>
-</div>
-<nav>
-	<ul class="list">
-		<?php include($TemplatePath.'sider.php'); ?>
-	</ul>
-</nav>
-<?php
-}
-?>
+</ul>
