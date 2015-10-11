39e1efa2d9a9673f8d2b42efe1d53a4801ea385a
LNDDYL <LNDDYL@users.noreply.github.com>
2015-10-11 17:36:59 +0800

    Update reset_password.php

--- language/zh-tw/reset_password.php
+++ language/zh-tw/reset_password.php
@@ -4,13 +4,13 @@ if (empty($Lang) || !is_array($Lang))
 	$Lang = array();
 
 $Lang = array_merge($Lang, array(
-	'Reset_Password' => '重置密碼',
-	'Page_Has_Expired' => '頁面已過期',
+	'Reset_Password' => '重設密碼',
+	'Page_Has_Expired' => '頁面已到期',
 	'New_Password' => '新密碼',
 	'Confirm_New_Password' => '再次輸入新密碼',
 	'VerificationCode_Error' => '驗證碼錯誤',
 	'Passwords_Inconsistent' => '密碼、重複密碼 輸入不一致',
-	'Forms_Can_Not_Be_Empty' => '密碼、重複密碼、驗證碼 必填',
-	'Reset_Password_Success' => '重置密碼成功',
-	'Reset_Password_Failure' => '重置密碼失敗'
+	'Forms_Can_Not_Be_Empty' => '密碼、重複密碼、驗證碼為必填欄位',
+	'Reset_Password_Success' => '重設密碼成功',
+	'Reset_Password_Failure' => '重設密碼失敗'
 	));
