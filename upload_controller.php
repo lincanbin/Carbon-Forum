3662db0640a655f5c54472af9b9593e5a936cd53
lincanbin <lincanbin@hotmail.com>
2015-10-11 15:02:32 +0800

    Remove useless line.

--- upload_controller.php
+++ upload_controller.php
@@ -1,7 +1,6 @@
 <?php
 //header('Access-Control-Allow-Origin: http://www.baidu.com'); //设置http://www.baidu.com允许跨域访问
 //header('Access-Control-Allow-Headers: X-Requested-With,X_Requested_With'); //设置允许的跨域header
-date_default_timezone_set("Asia/chongqing");
 //error_reporting(E_ERROR);
 $UploadConfig = json_decode(preg_replace("/\/\*[\s\S]+?\*\//", "", file_get_contents("includes/Uploader.config.json")), true);
 $action       = $_GET['action'];
