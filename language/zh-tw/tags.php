43a549170fbe6e9d6f511799a021824d21e32fb1
LNDDYL <LNDDYL@users.noreply.github.com>
2015-10-11 17:43:18 +0800

    Update tags.php

--- language/zh-tw/tags.php
+++ language/zh-tw/tags.php
@@ -6,6 +6,6 @@ if (empty($Lang) || !is_array($Lang))
 $Lang = array_merge($Lang, array(
 	'Followers' => '人收藏',
 	'Topics' => '個主題',
-	'Follow' => '關注本話題',
-	'Unfollow' => '取消關注本話題'
+	'Follow' => '跟隨此話題',
+	'Unfollow' => '取消跟隨此話題'
 	));
