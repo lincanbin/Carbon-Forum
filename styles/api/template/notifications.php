cae7af998705363b4c6234f427f28857e1547cfb
lincanbin <lincanbin@hotmail.com>
2015-11-06 19:28:00 +0800

    Truncate the content of notification messages.

--- styles/api/template/notifications.php
+++ styles/api/template/notifications.php
@@ -3,10 +3,12 @@ if (!defined('InternalAccess')) exit('{"Status": 0, "ErrorCode": "403", "ErrorMe
 foreach($ReplyArray as $Key => $Post)
 {
 	$ReplyArray[$Key]['PostFloor'] = 0;
+	$ReplyArray[$Key]['Content'] = strip_tags(mb_substr($Post['Content'], 0, 512, 'utf-8'),'<p><br><a>');;
 }
 foreach($MentionArray as $Key => $Post)
 {
 	$MentionArray[$Key]['PostFloor'] = 0;
+	$MentionArray[$Key]['Content'] = strip_tags(mb_substr($Post['Content'], 0, 512, 'utf-8'),'<p><br><a>');;
 }
 ?>
 {
