46389abd9cca3f05ce3c8610ffffdc3e32967af9
lincanbin <lincanbin@hotmail.com>
2015-11-09 00:08:59 +0800

    Notifications API.

--- styles/api/template/notifications.php
+++ styles/api/template/notifications.php
@@ -2,13 +2,13 @@
 if (!defined('InternalAccess')) exit('{"Status": 0, "ErrorCode": "403", "ErrorMessage": "403"}');
 foreach($ReplyArray as $Key => $Post)
 {
-	$ReplyArray[$Key]['PostFloor'] = 0;
-	$ReplyArray[$Key]['Content'] = strip_tags(mb_substr($Post['Content'], 0, 512, 'utf-8'),'<p><br><a>');;
+	$ReplyArray[$Key]['PostFloor'] = -1;
+	$ReplyArray[$Key]['Content'] = strip_tags(mb_substr($Post['Content'], 0, 256, 'utf-8'),'<p><br><a>');;
 }
 foreach($MentionArray as $Key => $Post)
 {
-	$MentionArray[$Key]['PostFloor'] = 0;
-	$MentionArray[$Key]['Content'] = strip_tags(mb_substr($Post['Content'], 0, 512, 'utf-8'),'<p><br><a>');;
+	$MentionArray[$Key]['PostFloor'] = -1;
+	$MentionArray[$Key]['Content'] = strip_tags(mb_substr($Post['Content'], 0, 256, 'utf-8'),'<p><br><a>');;
 }
 ?>
 {
