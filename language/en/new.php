26abf25d162e38ec4feb7bca666c5f7647212e78
Velody <dossantosgusmaosavio@gmail.com>
2015-12-25 10:32:09 -0200

    Simplified "can not"
    I simplified the " can not " into a " can't "
    Let me know if i'm wrong using " instead of '
    Because it was a necessary change.

--- language/en/new.php
+++ language/en/new.php
@@ -8,9 +8,9 @@ $Lang = array_merge($Lang, array(
 	'Add_Tags' => 'Add a tag (press Enter)',
 	'Submit' => ' Submit ',
 	'Posting_Too_Often' => 'Posting too often. Please try again later. ',
-	'Tags_Empty' => 'Tags can not be empty',
-	'Too_Long' => 'Title length can not exceed {{MaxTitleChars}} bytes, the content length can not exceed {{MaxPostChars}} bytes',
-	'Title_Empty' => 'Title can not be empty',
+	'Tags_Empty' => "Tags can't be empty",
+	'Too_Long' => "Title length can't exceed {{MaxTitleChars}} bytes, the content length can't exceed {{MaxPostChars}} bytes",
+	'Title_Empty' => "Title can't be empty",
 
 	'Tags' => 'Tags',
 	'Content' => 'Content'
