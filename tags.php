bc0ba44545d5d6e0498126e9745911d192c4f9a6
lincanbin <lincanbin@hotmail.com>
2016-04-19 22:07:41 +0800

    Fixed the bug.

--- tags.php
+++ tags.php
@@ -10,22 +10,23 @@ if ($Page > $TotalPage)
 if ($Page == 0)
 	$Page = 1;
 $TagsArray = array();
+$IsFavoriteArray = array();
 // UPDATE `carbon_tags` t SET t.Description=(SELECT d.Abstract FROM `carbon_dict` d WHERE d.Title = t.Name limit 1)
-if (!$TagsArray) {
-	$TagsArray = $DB->query('SELECT * 
-		FROM ' . $Prefix . 'tags 
-		WHERE IsEnabled=1 
-		ORDER BY TotalPosts DESC 
-		LIMIT ' . ($Page - 1) * $Config['TopicsPerPage'] . ',' . $Config['TopicsPerPage']);
 
-	if ($CurUserID){
-		$IsFavoriteArray = array_flip($DB->column("SELECT FavoriteID FROM " . $Prefix . "favorites 
-			Where UserID=".$CurUserID." and Type=2 and FavoriteID in (?)", 
-			ArrayColumn($TagsArray, 'ID')
-		));
-		//var_dump($IsFavoriteArray);
-	}
+$TagsArray = $DB->query('SELECT * 
+	FROM ' . $Prefix . 'tags 
+	WHERE IsEnabled=1 
+	ORDER BY TotalPosts DESC 
+	LIMIT ' . ($Page - 1) * $Config['TopicsPerPage'] . ',' . $Config['TopicsPerPage']);
+
+if ($CurUserID && $TagsArray){
+	$IsFavoriteArray = array_flip($DB->column("SELECT FavoriteID FROM " . $Prefix . "favorites 
+		Where UserID=".$CurUserID." and Type=2 and FavoriteID in (?)",
+		ArrayColumn($TagsArray, 'ID')
+	));
+	//var_dump($IsFavoriteArray);
 }
+
 $DB->CloseConnection();
 
 $PageTitle = $Page > 1 ? ' Page' . $Page . '-' : '';
