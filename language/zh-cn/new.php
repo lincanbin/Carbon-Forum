82916882bf04001b17fc2c26381abddec799b1df
lincanbin <lincanbin@hotmail.com>
2016-04-24 17:14:59 +0800

    i18n: Prohibited Content. $84

--- language/zh-cn/new.php
+++ language/zh-cn/new.php
@@ -9,6 +9,7 @@ $Lang = array_merge($Lang, array(
 	'Submit' => ' 发 布 ',
 	'Posting_Too_Often' => '您发帖过于频繁，请稍后再尝试',
 	'Tags_Empty' => '标签不能为空',
+	'Prohibited_Content' => '你发表的主题含有被禁止发表的内容',
 	'Too_Long' => '标题长度不能超过{{MaxTitleChars}}个字节，内容长度不能超过{{MaxPostChars}}个字节',
 	'Title_Empty' => '标题不能为空',
 
--- language/zh-tw/new.php
+++ language/zh-tw/new.php
@@ -9,6 +9,7 @@ $Lang = array_merge($Lang, array(
 	'Submit' => ' 發 佈 ',
 	'Posting_Too_Often' => '您的發言次數過於頻繁，請稍後再嘗試',
 	'Tags_Empty' => '標籤不能為空',
+	'Prohibited_Content' => '你發表的主題含有被禁止發表的內容',
 	'Too_Long' => '標題長度不能超過 {{MaxTitleChars}} 個位元組，內容長度不能超過 {{MaxPostChars}} 個位元組',
 	'Title_Empty' => '標題不能為空',
 
