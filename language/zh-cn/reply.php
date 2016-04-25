67d3a118506ead25483a51d3b9b091fbc15c1438
lincanbin <lincanbin@hotmail.com>
2016-04-25 22:37:26 +0800

    i18n: Prohibited Content.

--- language/zh-cn/reply.php
+++ language/zh-cn/reply.php
@@ -7,5 +7,6 @@ $Lang = array_merge($Lang, array(
 	'Posting_Too_Often' => '您发帖过于频繁，请稍后再尝试',
 	'Topic_Has_Been_Locked' => '此帖已被锁定，禁止回复',
 	'Too_Long' => '帖子内容长度不能超过{{MaxPostChars}}个字节',
-	'Content_Empty' => '内容不能为空'
+	'Content_Empty' => '内容不能为空',
+	'Prohibited_Content' => '你发表的主题含有被禁止发表的内容'
 	));
--- language/zh-tw/reply.php
+++ language/zh-tw/reply.php
@@ -7,5 +7,6 @@ $Lang = array_merge($Lang, array(
 	'Posting_Too_Often' => '您發言過於頻繁，請稍後再嘗試',
 	'Topic_Has_Been_Locked' => '該討論串已被鎖定，您無權回覆',
 	'Too_Long' => '回覆內容長度不能超過 {{MaxPostChars}} 個位元組',
-	'Content_Empty' => '內容不能為空白'
+	'Content_Empty' => '內容不能為空白',
+	'Prohibited_Content' => '你發表的主題含有被禁止發表的內容'
 	));
