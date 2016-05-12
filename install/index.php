c0440d416e6529d6c4a64e6c473c8edbe23781c4
Thomas Wilbur <alwaysonestepahead617@gmail.com>
2016-05-11 21:12:26 -0500

    Update index.php

--- install/index.php
+++ install/index.php
@@ -142,6 +142,7 @@ function GetNextSQL() {
 	<meta content="True" name="HandheldFriendly" />
 	<title>Install - Carbon Forum</title>
 	<link href="../styles/default/theme/style.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 </head>
 <body>
 	<!-- content wrapper start -->
