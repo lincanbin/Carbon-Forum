741b1e39bafa9f5f317d9b01d0f0f0a8810369eb
lincanbin <lincanbin@hotmail.com>
2016-06-18 18:38:36 +0800

    More abstract controller and routes. #191: remove old library.

--- includes/code/new.error.code.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-	'Default' => 103000,
-	'Title_Empty' => 103001,
-	'Too_Long' => 103002,
-	'Tags_Empty' => 103003,
-	'Posting_Too_Often' => 103004,
-	'Prohibited_Content' => 103005,
-);
