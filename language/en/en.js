af1761a40f2ea8aa18ffd24fc98b7e4f70bcaa1e
lincanbin <lincanbin@hotmail.com>
2016-12-23 00:32:30 +0800

    Translation problems.

--- language/en/en.js
+++ language/en/en.js
@@ -2,7 +2,7 @@
  * Carbon-Forum
  * https://github.com/lincanbin/Carbon-Forum
  *
- * Copyright 2006-2015 Canbin Lin (lincanbin@hotmail.com)
+ * Copyright 2006-2017 Canbin Lin (lincanbin@hotmail.com)
  * http://www.94cb.com/
  *
  * Licensed under the Apache License, Version 2.0:
