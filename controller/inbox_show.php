c8fb3595e702075f9039464212f8db3c2d8a1264
lincanbin <lincanbin@hotmail.com>
2017-03-15 20:16:08 +0800

    Private messages: add api which can get messages by inbox id. #97

--- controller/inbox_show.php
+++ controller/inbox_show.php
@@ -1,21 +1,31 @@
 <?php
-require(ServicePath . 'inbox.php');
-$InboxID = intval(Request('Get', 'inbox_id'));
-$UserInfo = array();
-if(preg_match('/^[1-9][0-9]*$/', $InboxID)) {
-	$DialogInfo = $DB->row('SELECT * FROM ' . PREFIX . 'inbox WHERE ID=:ID', array(
-		'ID' => $InboxID
-	));
-} else {
-	$UserInfo = $DB->row('SELECT * FROM ' . PREFIX . 'users WHERE ID=:ID', array(
-		'UserName' => $UserName
-	));
-	$InboxID = !empty($UserInfo) ? GetInboxID($UserInfo['ID']) : 0;
-	if ($InboxID) {
-		Redirect('inbox/' . $InboxID);
-	}
-}
+SetStyle('api', 'API');
+header("Content-Type: text/html; charset=utf-8");
+Auth(1, 0, true);
 
-if (empty($InboxID) || empty($DialogInfo)){
+$InboxID = intval(Request('Get', 'inbox_id'));
+$Page = intval(Request('Get', 'page', 1));
+$UserInfo = array();
+$DialogInfo = $DB->row('SELECT * FROM ' . PREFIX . 'inbox WHERE ID=:ID', array(
+	'ID' => $InboxID
+));
+
+if ($Page <= 0 || empty($DialogInfo)){
 	AlertMsg('404 Not Found', '404 Not Found', 404);
 }
+
+$MessagesArray = $DB->query('SELECT * FROM ' . PREFIX . 'messages 
+	WHERE IsDel = :IsDel AND InboxID=:InboxID ORDER BY Time DESC
+	LIMIT :Offset, :Number', array(
+		'IsDel' => 0,
+		'InboxID' => $InboxID,
+		'Offset' => ($Page - 1) * $Config['PostsPerPage'],
+		'Number' => $Config['PostsPerPage']
+));
+
+$DB->CloseConnection();
+
+// 页面变量
+$PageTitle   = 'Inbox';
+$ContentFile = $TemplatePath . 'reply.php';
+include($TemplatePath . 'layout.php');
