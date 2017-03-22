57d942b38acff609fc872803b23d5611eebdef19
lincanbin <lincanbin@hotmail.com>
2017-03-22 23:44:45 +0800

    API: get all messages in inbox . #97

--- controller/inbox_show.php
+++ controller/inbox_show.php
@@ -1,31 +1,37 @@
 <?php
 SetStyle('api', 'API');
 header("Content-Type: text/html; charset=utf-8");
-Auth(1, 0, true);
+Auth(1, 0, false);
 
 $InboxID = intval(Request('Get', 'inbox_id'));
 $Page = intval(Request('Get', 'page', 1));
 $UserInfo = array();
-$DialogInfo = $DB->row('SELECT * FROM ' . PREFIX . 'inbox WHERE ID=:ID', array(
-	'ID' => $InboxID
+$DialogInfo = $DB->row('SELECT * FROM ' . PREFIX . 'inbox WHERE ID=:ID AND (SenderID = :SenderID OR ReceiverID = :ReceiverID)', array(
+	'ID' => $InboxID,
+	'SenderID' => $CurUserID,
+	'ReceiverID' => $CurUserID,
 ));
 
 if ($Page <= 0 || empty($DialogInfo)){
 	AlertMsg('404 Not Found', '404 Not Found', 404);
 }
 
-$MessagesArray = $DB->query('SELECT * FROM ' . PREFIX . 'messages 
-	WHERE IsDel = :IsDel AND InboxID=:InboxID ORDER BY Time DESC
+$MessagesArray = $DB->query('SELECT UserID as ContactID, Content, Time FROM ' . PREFIX . 'messages 
+	WHERE IsDel = :IsDel AND InboxID = :InboxID ORDER BY Time DESC
 	LIMIT :Offset, :Number', array(
 		'IsDel' => 0,
 		'InboxID' => $InboxID,
 		'Offset' => ($Page - 1) * $Config['PostsPerPage'],
 		'Number' => $Config['PostsPerPage']
 ));
-
+foreach ($MessagesArray as &$Value) {
+	$Value['IsMe'] = ($Value['ContactID'] == $CurUserID);
+	$Value['ContactName'] = $DialogInfo['SenderID'] == $Value['UserID'] ? $DialogInfo['SenderName'] : $DialogInfo['ReceiverName'];
+	$Value['FormatTime'] = FormatTime($Value['Time']);
+}
 $DB->CloseConnection();
 
 // 页面变量
 $PageTitle   = 'Inbox';
-$ContentFile = $TemplatePath . 'reply.php';
+$ContentFile = $TemplatePath . 'inbox.php';
 include($TemplatePath . 'layout.php');
