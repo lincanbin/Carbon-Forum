bc09d13ea8f239903742bb2d50c2c100a2e11e89
lincanbin <lincanbin@hotmail.com>
2017-05-30 23:49:18 +0800

    Rename upload controller.

--- controller/upload.php
+++ controller/upload.php
@@ -2,25 +2,27 @@
 //header('Access-Control-Allow-Origin: http://www.baidu.com'); //设置http://www.baidu.com允许跨域访问
 //header('Access-Control-Allow-Headers: X-Requested-With,X_Requested_With'); //设置允许的跨域header
 //error_reporting(E_ERROR);
-$UploadConfig = json_decode(preg_replace("/\/\*[\s\S]+?\*\//", "", file_get_contents(LibraryPath . 'Uploader.config.json')), true);
-$action       = $_GET['action'];
+if (is_file(LibraryPath . 'Uploader.config.json')) {
+	$UploadConfig = JsonDecode(file_get_contents(LibraryPath . 'Uploader.config.json'));
+} else {
+	$UploadConfig = JsonDecode(file_get_contents(LibraryPath . 'Uploader.config.template.json'));
+}
+$action = $_GET['action'];
 
 switch ($action) {
 	case 'config':
 		$result = json_encode($UploadConfig);
 		break;
-	
 	/* 上传图片 */
 	case 'uploadimage':
-	/* 上传涂鸦 */
+		/* 上传涂鸦 */
 	case 'uploadscrawl':
-	/* 上传视频 */
+		/* 上传视频 */
 	case 'uploadvideo':
-	/* 上传文件 */
+		/* 上传文件 */
 	case 'uploadfile':
 		$result = include("upload_file.php");
 		break;
-	
 	/* 列出图片 */
 	case 'listimage':
 		$result = include("upload_list.php");
@@ -29,12 +31,11 @@ switch ($action) {
 	case 'listfile':
 		$result = include("upload_list.php");
 		break;
-	
 	/* 抓取远程文件 */
 	case 'catchimage':
 		$result = include("upload_crawler.php");
 		break;
-	
+
 	default:
 		$result = json_encode(array(
 			'state' => '请求地址出错'
