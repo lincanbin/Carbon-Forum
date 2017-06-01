ee3aa025e84c6da781b3801fe70a3634b50d7468
lincanbin <lincanbin@hotmail.com>
2017-06-01 17:14:33 +0800

    Fixed continuous integration.

--- library/PDO.class.php
+++ library/PDO.class.php
@@ -61,10 +61,8 @@ class DB
 					//For PHP 5.3.6 or lower
 					PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
 					PDO::ATTR_EMULATE_PREPARES => false,
-					PDO::ATTR_AUTOCOMMIT => false,
 					//长连接
 					//PDO::ATTR_PERSISTENT => true,
-					
 					PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 					PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true
 				)
