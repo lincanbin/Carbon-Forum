d353b910ea4cc643a3d7f7b7d2d63f6c8f143330
lincanbin <lincanbin@hotmail.com>
2017-12-27 01:53:40 +0800

    Typos. #245

--- console_run.sh
+++ console_run.sh
@@ -2,7 +2,7 @@ CURRENT_PATH=$(dirname $(readlink -f $0))
 SERVICE_PUSH_PATH=${CURRENT_PATH}/controller/console/console_push.sh
 LOG_PUSH_PATH=${CURRENT_PATH}/library/logs
 LOG_PUSH_FILE=${LOG_PUSH_PATH}/console_push.log
-mkdir -p "${LOG_PATH}"
+mkdir -p "${LOG_PUSH_PATH}"
 touch ${LOG_PUSH_FILE}
 chmod +x ${SERVICE_PUSH_PATH}
 nohup ${SERVICE_PUSH_PATH} >> ${LOG_PUSH_FILE} 2>&1 &
--- controller/console/console_push.sh
+++ controller/console/console_push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 work_path=$(dirname $(readlink -f $0))
 (
-    until /usr/bin/php ${work_path}/push; do
+    until /usr/bin/php ${work_path}/push.php; do
         echo "Carbon Forum push service crashed with exit code $?.  restarting... " >&2
         sleep 3
     done
